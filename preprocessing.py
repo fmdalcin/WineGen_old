5f9a54c9925bb775e774f880216a81e82713764d
Felipe Dalcin <fmdalcin@gmail.com>
2024-01-27 14:39:17 +1100

    Synonyms code added to preprocessing code

--- preprocessing.py
+++ preprocessing.py
@@ -11,14 +11,45 @@ def clean_rawdata():
 
 
     #Clean duplicates and deals with unmatched grape varieties
-    df = pd.read_csv("./raw_data/winemag-data-130k-v2_18Jan2024.csv", index_col=0)
+    df = pd.read_csv("./raw_data/wine_data_csv.csv", index_col=0)
     df.drop_duplicates(inplace=True)
-    columns_to_drop=['designation','region_2','taster_name','taster_twitter_handle','variety','region_1']
+    columns_to_drop=['designation','region_2','taster_name','taster_twitter_handle','region_1']
     df.drop(columns=columns_to_drop,inplace=True)
     df.dropna(inplace=True)
     df.drop(df[df['type']=='delete'].index,inplace=True)
-    df.to_csv("./raw_data/preprocessed_data.csv", index=False)
+    # df.to_csv("./raw_data/preprocessed_data.csv", index=False) #backup file moved to after resolve_synonyms()
     return df
+
+def resolve_synonyms(df:pd.DataFrame)->pd.DataFrame:
+    '''Reads the original data and implement standardised names for grape
+    varieties. The user does not know this happens in the background,
+    recommendations are still provided in the original variety.'''
+    # Loading wine synonyms input data from file created with unique grape for one or several wines
+    syns_raw = pd.read_csv("./raw_data/wine_synonyms_csv.csv", index_col=0)
+    # Extracting the single column from the synonyms files that has all synonyns for each row
+    all_grape_names = syns_raw.NAME_ALL.str.split(', ')
+    # The synonyms file has multiple rows (synonyms) that hasn't been unified (synonymised) properly.
+    # Here a table of synonyms is created to consolidate all synonyms.
+    syns = {}     # for each item: key will be main synonym, values will include all synonyms including the main one (used for key)
+    for row in all_grape_names:
+        flat_dict = [num for elem in list(syns.values()) for num in elem]
+        # checking if synonyms in each row are already present is the dictionary being created
+        # if not, it creates a key and values
+        if any(grape in flat_dict for grape in row) == False:
+            syns[row[0]] = row
+        # if yes, adds the new synonyns that don't exist yet in the list
+        else:
+            res = next((sub for sub in syns if any(grape in syns[sub] for grape in row) == True), None)
+            syns[res].extend([item for item in row if item not in syns[res]])
+    del flat_dict
+    # Using the unified synonym table, it populates the column for grape vaiety in the main data table
+    # with the main synonyms for each grape variety
+    df['variety_adj'] = df['variety'].apply(lambda grape: next((key for key, value in syns.items() if grape in value), None))
+    df.drop(columns=['variety'], inplace=True)
+    df.to_csv("./raw_data/preprocessed_data.csv", index=False)
+
+    return df
+
 
 def scaling(df):
     '''Scales the columns of the dataset in an X and reference.'''
